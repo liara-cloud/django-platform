6aa19ed0a00386fdaa5de5b652c70e88f233e237
Yazdan Ranjbar <Yazdan_ra@icloud.com>
2021-01-16 16:47:32 +0330

    get `settings/__init__py` besides of `settings.py`
    Some projects use python packages for settings and separate its components.

--- lib/append-settings.sh
+++ lib/append-settings.sh
@@ -1,5 +1,5 @@
 # Find settings file
-SETTINGS_FILE=$(find . -name 'settings.py')
+SETTINGS_FILE=$(find . -name 'settings.py' -o 'settings/__init__.py')
 
 # Append our custom settings
 cat /usr/local/lib/liara-django/liara_django_settings_temp.py >> $SETTINGS_FILE
