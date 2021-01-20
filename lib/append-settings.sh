c94459a2f06a218a66c0a0f99c6ae0150cfbf304
Yazdan Ranjbar <Yazdan_ra@icloud.com>
2021-01-20 13:07:45 +0330

    Update append-settings.sh

--- lib/append-settings.sh
+++ lib/append-settings.sh
@@ -1,5 +1,5 @@
 # Find settings file
-SETTINGS_FILE=$(find . -name 'settings.py' -o 'settings/__init__.py')
+SETTINGS_FILE=$(find . -name 'settings.py' | grep -z . || find . -wholename "**/settings/__init__.py")
 
 # Append our custom settings
 cat /usr/local/lib/liara-django/liara_django_settings_temp.py >> $SETTINGS_FILE
