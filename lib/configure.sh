e494d6a64b6d91ae76c5671e4e164b0eae203e01
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2021-01-22 20:41:58 +0330

    Fix showing errors

--- lib/configure.sh
+++ lib/configure.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-set -e
+source /usr/local/lib/liara/util.sh
 
 # Append our custom settings
 if [ "$__DJANGO_MODIFY_SETTINGS" = "true" ]; then
-  cat /usr/local/lib/liara/settings-template.py >> $(/usr/local/lib/liara/find-settings.sh)
+  settings_file=$(/usr/local/lib/liara/find-settings.sh)
+
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    err
+    err "Error: We have found multiple settings files for your Django app:"
+    err "$(echo "$settings_file" | awk '{print "  " NR  ") " $s}')"
+    err
+    err "Create a liara.json file in your project's root directory with the following content:"
+    err
+    err "{
+  \"django\": {
+    \"settingsFile\": \"my-project/settings.py\"
+  }
+}"
+    err
+    err "You should put your actual settings file path into the above file."
+    err "> Read more: https://docs.liara.ir/errors/django-settings-file"
+    err
+    exit $exit_code
+  fi
+
+  cat /usr/local/lib/liara/settings-template.py >> "$settings_file"
 fi
+
+set -e
 
 if [ "$__DJANGO_COLLECTSTATIC" = "true" ]; then
   echo '> Running collectstatic...'
--- lib/find-settings.sh
+++ lib/find-settings.sh
@@ -10,19 +10,7 @@ RESULT=$(grep -l -R WSGI_APPLICATION --include \*.py --exclude-dir venv)
 
 # https://stackoverflow.com/a/60811975/6390238
 if [ "$(echo "$RESULT" | wc -l)" -gt "1" ]; then
-  echo "Error: We have found multiple settings files for your Django app:"
-  echo "$RESULT" | awk '{print "  " NR  ") " $s}'
-  echo
-  echo "Create a liara.json file in your project's root directory with the following content:"
-  echo "{
-  \"django\": {
-    \"settingsFile\": \"my-project/settings.py\"
-  }
-}"
-  echo "You should put your actual settings file path into the above file."
-  echo
-  echo "> Read more: https://docs.liara.ir/errors/django-settings-file"
-  echo
+  echo "$RESULT"
   exit 2
 fi
 
