ef383a580aa67415346ac5dc4160382dc40162ba
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2021-04-13 13:14:17 +0430

    Add support for Python 3.8 and 3.9

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-docker build -t liararepo/django-platform .
+docker build -t liararepo/django-platform:python3.7 -f Dockerfile.python3.7 .
+docker build -t liararepo/django-platform:python3.8 -f Dockerfile.python3.8 .
+docker build -t liararepo/django-platform:python3.9 -f Dockerfile.python3.9 .
